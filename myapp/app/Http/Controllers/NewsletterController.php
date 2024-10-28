c29977c6f462b135ce48ea57dfc3d2dc9cc56019
Petronia07 <164915372+Petronia07@users.noreply.github.com>
2024-10-28 12:48:51 +0100

    Update NewsletterController.php

--- myapp/app/Http/Controllers/NewsletterController.php
+++ myapp/app/Http/Controllers/NewsletterController.php
@@ -22,14 +22,11 @@ class NewsletterController extends Controller
         return response()->json(['message' => 'Vous êtes maintenant abonné à la newsletter !'], 201);
     }
 
-    // Méthode pour envoyer la newsletter
     public function send(Request $request)
     {
-        // Récupérer tous les abonnés (vous pouvez filtrer les abonnés actifs si nécessaire)
         $subscribers = Subscriber::all();
 
         foreach ($subscribers as $subscriber) {
-            // Envoi de l'e-mail
             Mail::to($subscriber->email)->send(new NewsletterMail($subscriber));
         }
 
